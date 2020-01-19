891d01763df9760dae2a35022065f14c50fff988
Lihe Chen <lihe.chen@gmail.com>
2020-01-18 19:32:31 -0600

    added login links to example

--- ClientApp/src/app/example/example.module.ts
+++ ClientApp/src/app/example/example.module.ts
@@ -8,16 +8,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
+import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 
 
 const routes: Routes = [
-  { path: '', component: ExampleComponent }
+  { path: '', component: ExampleComponent, canActivate: [AuthorizeGuard]  }
 ];
 
 @NgModule({
   declarations: [ExampleComponent],
   imports: [
     CommonModule,
+    ApiAuthorizationModule,
     RouterModule.forChild(routes),
     LayoutModule,
     MatToolbarModule,
